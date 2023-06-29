7285c8feccc5431a96d599445ed40ab1a1af3ba2
アシア　ロスチャスター <asiaroschester@ashiarosuchasutanoMacBook-Air.local>
2023-06-29 13:31:29 +0900

    userモデルの作成はアイコン部分以外完成？

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def home
   end
+
+  def show_profile
+    @user = current_user
+  end
+
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to users_show_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduce)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'users#home'
+  get 'users/show_profile', to: 'users#show_profile'
+  get 'users/show', to: 'users#show'
+  get 'users/edit_account', to: 'users#edit'
+  patch 'users/edit', to: 'users#update'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
