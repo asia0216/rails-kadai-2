f5636f4a2d1f6366004646c8df30d0c57ac0cc07
アシア　ロスチャスター <asiaroschester@ashiarosuchasutanoMacBook-Air.local>
2023-06-30 13:41:06 +0900

    editの問題があった部分が解決？(createはまだ)

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  root 'users#home'
+
   get 'rooms/index', to: 'rooms#index'
   get 'rooms/new', to: 'rooms#new'
   post 'rooms/new', to: 'rooms#create'
   get 'rooms/show', to: 'rooms#show'
-  get 'rooms/edit', to: 'rooms#edit'
+  
+  resources :rooms, :only => :edit
 
-  root 'users#home'
   get 'users/show_profile', to: 'users#show_profile'
   get 'users/show', to: 'users#show'
   get 'users/edit_account', to: 'users#edit'
   patch 'users/edit', to: 'users#update'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
